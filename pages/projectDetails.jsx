0438bc8432599bbd8f852cc90920bfdbc31c7cf7
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-06-27 01:49:21 +0500

    fixed an issue for no projects inserted by user

--- pages/projectDetails.jsx
+++ pages/projectDetails.jsx
@@ -70,94 +70,93 @@ const ProjectDetails = (props) => {
                     <Navbar />
                 </div>
             </div>
-
             <br />
             <br />
             <br />
             <br />
-
             {(status) ? (
                 <div className="container">
                     {(loading) ? (
                         <>
-                            <div>
-                                <h1 className="text-center">{firestoreData[0].ProjectName}</h1>
-                                <p className="text-center text-info">All the project details can be customized and updated according to your choices.</p>
-                            </div>
-                            <div title={`This is a sample preview of your project dear ${signedInUserData.name}`}>
-                                <table className="table table-bordered">
-                                    <thead>
-                                        <tr>
-                                            <th colSpan={5}><h2><i className="fas fa-list-alt fa-lg mr-3" style={{ color: "#48dafd" }}></i>&nbsp;&nbsp; {firestoreData[0].ProjectName}</h2></th>
-                                        </tr>
-                                        <tr>
-                                            <th scope="col">Task name</th>
-                                            <th scope="col">Assignee</th>
-                                            <th scope="col">Due date</th>
-                                            <th scope="col">Priority</th>
-                                            <th scope="col">Status</th>
-                                        </tr>
-                                    </thead>
-                                    <>
-                                        {/* {(stage)?(
-
-                                ):(
-
-                                )} */}
-
-
-                                        {/* This matters */}
-                                        {firestoreData[0].ProjectStages.map((s, i) => {
-                                            return <tbody key={i}>
+                            {(firestoreData.length == 0) ? (
+                                <div>
+                                    <h1 className="mt-3 text-info text-center">Please Create any project first to see its details</h1>
+                                    <p className="btn btn-link border"><Link href="/new">Create New Project</Link></p>
+                                </div>
+                            ) : (
+                                <div>
+                                    <div>
+                                        <h1 className="text-center">{firestoreData[0].ProjectName}</h1>
+                                        <p className="text-center text-info">All the project details can be customized and updated according to your choices.</p>
+                                    </div>
+                                    <div title={`This is a sample preview of your project dear ${signedInUserData.name}`}>
+                                        <table className="table table-bordered">
+                                            <thead>
                                                 <tr>
-                                                    <th scope="row" colSpan={5}><h5><i className="fas fa-chevron-down mr-3"></i>&nbsp; {s}</h5></th>
+                                                    <th colSpan={5}><h2><i className="fas fa-list-alt fa-lg mr-3" style={{ color: "#48dafd" }}></i>&nbsp;&nbsp; {firestoreData[0].ProjectName}</h2></th>
                                                 </tr>
-                                                {(firestoreData[0].ProjectTasks.length == 0) ? (
-                                                    <tr>
-                                                        <th scope="row"><i className="far fa-check-circle fa-lg"></i>&nbsp;&nbsp;</th>
-                                                        <td>&nbsp;&nbsp;</td>
-                                                        <td>&nbsp;&nbsp;</td>
-                                                        <td>&nbsp;&nbsp;</td>
-                                                        <td>&nbsp;&nbsp;</td>
-                                                    </tr>
-                                                ) : (
-                                                    firestoreData[0].ProjectTasks.map((v, i) => {
-                                                        return <tr key={i}>
-                                                            {(v.taskSection == s) ? (
-                                                                <>
-
-                                                                    <th scope="row"><i className="far fa-check-circle fa-lg"></i>&nbsp;&nbsp;{v.taskName}</th>
-                                                                    {(v.taskAssignee == "") ? (
-                                                                        <td><i className="fas fa-user-circle fa-2x text-primary"></i></td>
-                                                                    ) : (
-                                                                        <td>{v.taskAssignee}</td>
-                                                                    )}
-
-                                                                    <td>{v.taskDue}</td>
-                                                                    {(v.taskPriority == "High") ? (
-                                                                        <td><button type="button" className="btn btn-danger btn-rounded">{v.taskPriority}</button></td>
-                                                                    ) : (v.taskPriority == "Medium") ? (
-                                                                        <td><button type="button" className="btn btn-warning btn-rounded">{v.taskPriority}</button></td>
-                                                                    ) : (v.taskPriority == "Low") ? (
-                                                                        <td><button type="button" className="btn btn-info btn-rounded">{v.taskPriority}</button></td>
-                                                                    ) : (
-                                                                        <td></td>
-                                                                    )}
-                                                                    <td><h5>{v.taskSection}</h5></td>
-                                                                </>
-                                                            ) : (
-                                                                <></>
-                                                            )}
+                                                <tr>
+                                                    <th scope="col">Task name</th>
+                                                    <th scope="col">Assignee</th>
+                                                    <th scope="col">Due date</th>
+                                                    <th scope="col">Priority</th>
+                                                    <th scope="col">Status</th>
+                                                </tr>
+                                            </thead>
+                                            <>
+                                                {/* This matters */}
+                                                {firestoreData[0].ProjectStages.map((s, i) => {
+                                                    return <tbody key={i}>
+                                                        <tr>
+                                                            <th scope="row" colSpan={5}><h5><i className="fas fa-chevron-down mr-3"></i>&nbsp; {s}</h5></th>
                                                         </tr>
-                                                    })
-                                                )}
-                                                )
-                                            </tbody>
-                                        })}
-                                        {/* This matters */}
-                                    </>
-                                </table>
-                            </div>
+                                                        {(firestoreData[0].ProjectTasks.length == 0) ? (
+                                                            <tr>
+                                                                <th scope="row"><i className="far fa-check-circle fa-lg"></i>&nbsp;&nbsp;</th>
+                                                                <td>&nbsp;&nbsp;</td>
+                                                                <td>&nbsp;&nbsp;</td>
+                                                                <td>&nbsp;&nbsp;</td>
+                                                                <td>&nbsp;&nbsp;</td>
+                                                            </tr>
+                                                        ) : (
+                                                            firestoreData[0].ProjectTasks.map((v, i) => {
+                                                                return <tr key={i}>
+                                                                    {(v.taskSection == s) ? (
+                                                                        <>
+                                                                            <th scope="row"><i className="far fa-check-circle fa-lg"></i>&nbsp;&nbsp;{v.taskName}</th>
+                                                                            {(v.taskAssignee == "") ? (
+                                                                                <td><i className="fas fa-user-circle fa-2x text-primary"></i></td>
+                                                                            ) : (
+                                                                                <td>{v.taskAssignee}</td>
+                                                                            )}
+
+                                                                            <td>{v.taskDue}</td>
+                                                                            {(v.taskPriority == "High") ? (
+                                                                                <td><button type="button" className="btn btn-danger btn-rounded">{v.taskPriority}</button></td>
+                                                                            ) : (v.taskPriority == "Medium") ? (
+                                                                                <td><button type="button" className="btn btn-warning btn-rounded">{v.taskPriority}</button></td>
+                                                                            ) : (v.taskPriority == "Low") ? (
+                                                                                <td><button type="button" className="btn btn-info btn-rounded">{v.taskPriority}</button></td>
+                                                                            ) : (
+                                                                                <td></td>
+                                                                            )}
+                                                                            <td><h5>{v.taskSection}</h5></td>
+                                                                        </>
+                                                                    ) : (
+                                                                        <></>
+                                                                    )}
+                                                                </tr>
+                                                            })
+                                                        )}
+                                                        )
+                                                    </tbody>
+                                                })}
+                                                {/* This matters */}
+                                            </>
+                                        </table>
+                                    </div>
+                                </div>
+                            )}
                         </>
                     ) : (
                         <>
